ea686053f6d52bec71b634fbfc6f920473e80c5b
Ian Lloyd Dela Cruz <icemooch@hotmail.com>
2015-05-31 20:08:48 +0800

    Update mask-decimate.py

--- mask-decimate.py
+++ mask-decimate.py
@@ -16,6 +16,9 @@ import bmesh
 from bpy.props import *
 
 def mask_to_vertex_group(obj, name):
+    wm = bpy.context.window_manager
+    maskcount = 0
+    
     vgroup = obj.vertex_groups.new(name)
     bpy.ops.object.vertex_group_set_active(group=vgroup.name)
     
@@ -25,20 +28,29 @@ def mask_to_vertex_group(obj, name):
     if deform_layer is None: deform_layer = bm.verts.layers.deform.new()
     mask = bm.verts.layers.paint_mask.active
     if mask is not None:
+        wm.mask_detect = True
         for v in bm.verts:
             if v[mask] == 1:
+                maskcount += 1
                 v.select = True
                 v[deform_layer][vgroup.index] = 1.0
             else:
-                v.select = False                   
+                v.select = False
     else:
-        for v in bm.verts:
-            v[deform_layer][vgroup.index] = 1.0
-  
+        wm.mask_detect = False                                  
+    
+    if maskcount == 0: wm.mask_detect = False
+ 
     bm.to_mesh(obj.data)
     bm.free()
     pass
     return vgroup
+
+def rem_vgroup(name):
+
+    if name in bpy.context.active_object.vertex_groups:
+        bpy.ops.object.vertex_group_set_active(group=name)            
+        bpy.ops.object.vertex_group_remove(all=False)   
 
 class MaskDecimate(bpy.types.Operator):
     '''Decimate Masked Areas'''
@@ -60,9 +72,18 @@ class MaskDecimate(bpy.types.Operator):
             dynatopoEnabled = True
         else:
             dynatopoEnabled = False
+            
+        rem_vgroup(vname)            
         
         #convert mask to vgroup
         mask_to_vertex_group(activeObj, vname)
+
+        if wm.mask_detect == True:
+            self.report({'WARNING'}, "Masked Areas Present!")
+        else:
+            self.report({'WARNING'}, "No Masked Areas!")
+            rem_vgroup(vname)            
+            return {'FINISHED'}                  
         
         #place decimate modifier
         md = bpy.context.active_object.modifiers.new(modnam, 'DECIMATE')
@@ -70,12 +91,11 @@ class MaskDecimate(bpy.types.Operator):
         md.ratio = wm.maskdecimate_str
 
         #apply modifier and remove vgroup
-        bpy.ops.object.mode_set(mode='OBJECT')
-        bpy.ops.object.modifier_apply(apply_as='DATA', modifier=modnam)
-        
-        if vname in bpy.context.active_object.vertex_groups:
-            bpy.ops.object.vertex_group_set_active(group=vname)            
-            bpy.ops.object.vertex_group_remove(all=False)   
+        if wm.maskdecimate_apply == True:
+            bpy.ops.object.mode_set(mode='OBJECT')
+            bpy.ops.object.modifier_apply(apply_as='DATA', modifier=modnam)
+            
+            rem_vgroup(vname) 
         
         bpy.ops.object.mode_set(mode='SCULPT')
         if dynatopoEnabled:
@@ -99,11 +119,15 @@ class MaskDecimationPanel(bpy.types.Panel):
         row_sw.operator("mask.decimate", "Mask Decimate")
         row_sw = layout.row(align=False)
         row_sw.prop(wm, "maskdecimate_str", "Decimation Strength")
+        row_sw = layout.row(align=False)
+        row_sw.prop(wm, "maskdecimate_apply", "Apply Decimate")          
        
 def register():
     bpy.utils.register_module(__name__)
     
-    bpy.types.WindowManager.maskdecimate_str = FloatProperty(min = 0, max = 1, step = 0.1, precision = 3, default = 1)
+    bpy.types.WindowManager.maskdecimate_str = FloatProperty(min = 0, max = 1, step = 0.1, precision = 3, default = 0.95)
+    bpy.types.WindowManager.mask_detect = BoolProperty(default=False)
+    bpy.types.WindowManager.maskdecimate_apply = BoolProperty(default=True)    
   
 def unregister():
     bpy.utils.unregister_module(__name__)
